13ea6b3f0ee365a2143fa32a83993718ce2a1e1e
marieecaloc757 <marieecaloc757@gmail.com>
2025-05-26 22:37:32 +0000

    Add 43.py

---
+++ src/43.py
@@ -0,0 +1,9 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def load_data():
+    # This function loads your data and returns it.
+    pass
+
+if __name__ == "__main__":
+    print("Please run 'load_data()' to get the data.")
